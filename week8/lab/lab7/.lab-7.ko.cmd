b92e55f83feb8440550adffbb156ec77ab6257b8
Alibek120699 <sayakalibek1@gmail.com>
2020-03-28 11:43:37 +0600

    lab8

--- week8/lab/lab7/.lab-7.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/aleke/sysprog/SystemProgramming/week8/lab/lab7/lab-7.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/aleke/sysprog/SystemProgramming/week8/lab/lab7/lab-7.ko /home/aleke/sysprog/SystemProgramming/week8/lab/lab7/lab-7.o /home/aleke/sysprog/SystemProgramming/week8/lab/lab7/lab-7.mod.o ;  true
--- week8/lab/lab7/.tmp_versions/lab-7.mod
+++
@@ -1,3 +0,0 @@
-/home/aleke/sysprog/SystemProgramming/week8/lab/lab7/lab-7.ko
-/home/aleke/sysprog/SystemProgramming/week8/lab/lab7/lab-7.o
-
